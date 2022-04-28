1e6f85f77ed0f2dfd3e3670b75587f6fc3628bee
François <ledune.francois@hotmail.fr>
2022-04-28 10:53:14 +0200

    Commentage en aide mémoire

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,11 +1,14 @@
+//Récupéré les données de l'API
 async function getArticles() {
     const articlescatch = await fetch("http://localhost:3000/api/products");
     return await articlescatch.json();
 }
 
+
 async function showArticles() {
     const articles = await getArticles();
     articles.forEach(article => {
+        //Création des emplacements pour les éléments de l'API
         const articleContainer = document.getElementById("items");
         const articleLink = document.createElement("a");
         const articleTag = document.createElement("article");
@@ -13,12 +16,14 @@ async function showArticles() {
         const titleTag = document.createElement("h3");
         const articleDescritpion = document.createElement("p");
 
+        //Indication de l'équivalence des constantes par rapport au données de l'API
         articleLink.href = "./product.html?id=" + article["_id"];
         imageTag.src = article.imageUrl;
         titleTag.classList.add("articleName");
         titleTag.textContent = article.name;
         articleDescritpion.textContent = article.description;
        
+        //Résultat des constantes
         articleLink.append(articleTag)
         articleTag.append(imageTag);
         articleTag.append(titleTag);
@@ -28,13 +33,13 @@ async function showArticles() {
         console.log(article);
     });
 
+    //En cas d'erreur
     try {
         nonExistentFunction();
       } catch (error) {
         console.error(error);
-
       }
-    
 };
 
 showArticles();
+
